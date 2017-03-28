07ccc8665d150ff25df47dcf10f44f961e499839
chris48s <chris.shaw480@gmail.com>
2017-03-28 17:12:11 +0100

    init

---
+++ scraper.py
@@ -0,0 +1,12 @@
+from dc_base_scrapers.arcgis_scraper import ArcGisScraper
+
+
+stations_url = "http://mapping.thanet.gov.uk/arcgis/rest/services/Open_Data/Polling_Stations/MapServer/0/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
+districts_url = "http://mapping.thanet.gov.uk/arcgis/rest/services/Open_Data/Polling_Boundaries/MapServer/0/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
+council_id = 'E07000114'
+
+
+stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper.scrape()
+districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
